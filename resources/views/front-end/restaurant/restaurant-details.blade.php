6a14a73efb8a4f96c8ab4af8b67961e886cd40d8
SajalKundu <sajalkundu098@gmail.com>
2019-09-13 13:27:40 +0600

    Static Translation complete

--- resources/views/front-end/restaurant/restaurant-details.blade.php
+++ resources/views/front-end/restaurant/restaurant-details.blade.php
@@ -13,7 +13,7 @@
           {{ $store->name_arabic }}
         @endif
       </span>
-      <span class="store-rate-title">Store Rate</span>
+      <span class="store-rate-title">{{ __('content.store_rate') }}</span>
       <span class="store-rating">
         @for ($i=0; $i < 3; $i++)
         <i class="fa fa-star"></i>
@@ -23,7 +23,7 @@
       </span>
     </div>
     <div class="restaurant-details-div">
-      <h1 class="text-left">Restaurant Details</h1>
+      <h1 class="text-left">{{ __('content.restuarent_details') }}</h1>
       <div class="text-left single-store-description">
         @if(app()->getLocale() == 'en')
           {{ $store->description }}
@@ -31,7 +31,7 @@
           {{ $store->arabic_description }}
         @endif
       </div>
-      <h1 class="text-left">Menu</h1>
+      <h1 class="text-left">{{ __('content.menu') }}</h1>
       <div class="restaurant-details-mother">
         {{-- @foreach($multiple_images as $multiple_image)
         <div class="restaurant-details-box">
@@ -59,7 +59,11 @@
         <p class="text-left">2.05 Km</p>
       </div>
       <div class="store-status text-left">
-        <i class="fa fa-circle online"></i><span class="store-status-text">Store is Open (working time)</span>
+        <i class="fa fa-circle online"></i>
+        <span class="store-status-text">
+          {{ __('content.store_is_open') }}
+          {{ __('content.store_is_closed') }}
+        </span>
       </div>
       {{-- <div class="order-button text-center">
         <a href="{{ route('order-details') }}"><button class="btn btn-success" type="button" name="button">Order Menu</button></a>
--- resources/views/front-end/store/store-place-order.blade.php
+++ resources/views/front-end/store/store-place-order.blade.php
@@ -13,7 +13,7 @@
           <div class="product-image-box shadow">
             <img src="{{ asset('front-end-assets/images/b5.jpg') }}" alt="">
           </div>
-          <span class="service-name text-left">Service Name <br>Available units 10 </span>
+          <span class="service-name text-left">Service Name <br>{{ __('content.available_unit') }} 10 </span>
           <span class="total-amount">3</span>
           <span class="product-amount">30SR</span>
         </div>
@@ -21,36 +21,36 @@
           <div class="product-image-box shadow">
             <img src="{{ asset('front-end-assets/images/b5.jpg') }}" alt="">
           </div>
-          <span class="service-name">Service Name <br>Available units 10 </span>
+          <span class="service-name">Service Name <br>{{ __('content.available_unit') }} 10 </span>
           <span class="total-amount">3</span>
           <span class="product-amount">30SR</span>
         </div>
       </div>
       <div class="payment-details-div shadow">
         <div class="payment-details-box">
-          <div class="payment-title"> <h1>Payment Method</h1> </div>
+          <div class="payment-title"> <h1>{{ __('content.payment_method') }}</h1> </div>
           <div class="payment-description"> <p class="font-p">COD</p> </div>
         </div>
         <div class="payment-details-box">
-          <div class="delivery-title"> <h1>Delivery Method</h1> </div>
+          <div class="delivery-title"> <h1>{{ __('content.delivery_method') }}</h1> </div>
           <div class="delivery-description"> <p class="font-p">Customer Location</p></div>
           <div class="delivery-cost"> <p class="font-p">20SR</p></div>
         </div>
         <div class="payment-details-box">
-          <div class="payment-title"> <h1>Date</h1> </div>
+          <div class="payment-title"> <h1>{{ __('content.date') }}</h1> </div>
           <div class="payment-description"> <p class="font-p">20/10/2019</p> </div>
         </div>
         <div class="payment-details-box">
-          <div class="payment-title"> <h1>Time</h1> </div>
+          <div class="payment-title"> <h1>{{ __('content.time') }}</h1> </div>
           <div class="payment-description"> <p class="font-p">10am - 12pm</p> </div>
         </div>
         <div class="payment-details-box">
-          <div class="promo-code"> <h1>Promo Code</h1> </div>
+          <div class="promo-code"> <h1>{{ __('content.promo_code') }}</h1> </div>
           <div class="promo-input-div"> <input class="promo-input" type="text" name="" value=""> </div>
-          <div class="promo-button"> <button class="btn btn-success" type="button" name="button">Apply</button> </div>
+          <div class="promo-button"> <button class="btn btn-success" type="button" name="button">{{ __('content.apply') }}</button> </div>
         </div>
         <div class="place-order-button-div">
-          <button class="btn btn-success place-order-button" type="button" name="button">Add Cart</button>
+          <button class="btn btn-success place-order-button" type="button" name="button">{{ __('content.add_cart') }}</button>
         </div>
       </div>
     </div>
