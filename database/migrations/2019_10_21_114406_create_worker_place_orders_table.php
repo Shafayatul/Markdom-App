04fcbc3fbf87f929029ffc7e5df239f2a77daa12
SajalKundu <sajalkundu098@gmail.com>
2019-10-21 16:01:26 +0600

    worker place order migration file modify

--- database/migrations/2019_10_21_114406_create_worker_place_orders_table.php
+++ database/migrations/2019_10_21_114406_create_worker_place_orders_table.php
@@ -14,7 +14,7 @@ class CreateWorkerPlaceOrdersTable extends Migration
     public function up()
     {
         Schema::create('worker_place_orders', function (Blueprint $table) {
-            $table->integer('id');
+            $table->increments('id');
             $table->integer('user_id')->nullable();
             $table->integer('address_id')->nullable();
             $table->integer('schedule_time_id')->nullable();
